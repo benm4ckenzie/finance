b83865721cab21505d1e68fa2bedc57deaaa9796
benm4ckenzie <b_m@me.com>
2021-04-03 12:06:04 +0000

    Creates Payment table in database

--- monthly_budget/models.py
+++ monthly_budget/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    recipient = models.CharField(max_length=50, null=False, blank=False)
+    has_paid = models.BooleanField(null=False, blank=False, default=False)
