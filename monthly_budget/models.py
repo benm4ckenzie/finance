bee897b2d5576b8f3bf4dd112bd31bf115edd1e0
benm4ckenzie <b_m@me.com>
2021-04-03 15:35:21 +0000

    Adds migration and corrects errors

--- monthly_budget/models.py
+++ monthly_budget/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Payment(models.Model):
-    recipient = models.CharField(max_length=50, null=False, blank=False)
+    payment_category = models.CharField(max_length=50, null=False, blank=False)
+    payment_account = models.CharField(max_length=50, null=False, blank=False, default="Starling (Joint)")
+    payment_owner = models.CharField(max_length=50, null=False, blank=False, default="Household")
+    payment_date = models.IntegerField(null=False, blank=False)
+    instalment_amount = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    payment_recipient = models.CharField(max_length=50, null=False, blank=False)
     has_paid = models.BooleanField(null=False, blank=False, default=False)
+    payment_completion_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.payment_recipient
--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def get_monthly_budget_list(request):
     return render(request, 'monthly_budget/monthly_budget_list.html')
