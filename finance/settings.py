ec89d198e4dbb2d50d0efa99745b7e0e6507d5a4
benm4ckenzie <b_m@me.com>
2021-04-09 21:50:53 +0000

    Adds requirements.txt file following set up of heroku

--- finance/settings.py
+++ finance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'finance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://pbweizgcakuztm:928b71f94440f1e3f759dc9911c22884b17200c0fffcc2ef6a5416ab7366fac4@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d3vgenb2hv2nqm')
 }
 
 
