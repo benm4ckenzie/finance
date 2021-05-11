32534dec521008f109b33302f066e89789f1fa9b
benm4ckenzie <b_m@me.com>
2021-05-11 20:01:30 +0000

    added static files heroku settings to settings.py

--- finance/settings.py
+++ finance/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -131,6 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_ROOT = '/workspace/finance/monthly_budget/static'
+
+django_heroku.settings(locals())
