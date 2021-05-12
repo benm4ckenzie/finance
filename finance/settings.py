b1590c352557e4d7aca719b9aeeaaa822c338a33
benm4ckenzie <b_m@me.com>
2021-05-12 13:38:14 +0000

    CHnages made to settings

--- finance/settings.py
+++ finance/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'monthly_budget/static/monthly_budget'),
+    BASE_DIR / 'monthly_budget/static/monthly_budget'
 )
 
 django_heroku.settings(locals())
