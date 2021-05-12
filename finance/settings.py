d926cbea9fd9f6a064686b5699dbe5054124cb8b
benm4ckenzie <b_m@me.com>
2021-05-12 13:21:21 +0000

    corrected file path for static files

--- finance/settings.py
+++ finance/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'monthly_budget/static/monthly_budget'),
 )
 
 django_heroku.settings(locals())
